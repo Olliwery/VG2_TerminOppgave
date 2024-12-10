3916c634158ecff22b9cf268db1181acbba59475
Olliwery <145795936+Olliwery@users.noreply.github.com>
2024-12-10 10:47:57 +0100

    p

--- TerminOppgaveVG2/static/javascript.js
+++ TerminOppgaveVG2/static/javascript.js
@@ -10,3 +10,18 @@ function clickCookie(){
     Cookies = Cookies + 1
     printCookies()
 }
+
+
+function openPopup(nr) {
+    let popup = document.getElementById(nr);
+    let overlay = document.getElementById('overlay');
+    popup.classList.add("open-popup"); // Show popup
+    overlay.classList.add("show");    // Show and blur overlay
+}
+
+function closePopup(nr) {
+    let popup = document.getElementById(nr);
+    let overlay = document.getElementById('overlay');
+    popup.classList.remove("open-popup"); // Hide popup
+    overlay.classList.remove("show");    // Remove blur overlay
+}
